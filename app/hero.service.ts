2f156c1a76f5b11e624106aab634bd9c9361ae1f
nadavperetz <nadavperetz@gmail.com>
2016-12-07 15:56:09 -0200

    Using Promise

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,7 +4,12 @@ import {HEROES} from "./mock-heroes"
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
-  } // stub
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(resolve, 2000)) // delay 2 seconds
+      .then(() => this.getHeroes());
+  }
 }
